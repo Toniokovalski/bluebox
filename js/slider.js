a24494a29de6bee901c0025570e4b6f0b55f8696
Toniokovalski <toniokovalski@gmail.com>
2015-09-09 21:05:05 +0300

    added slider, added services block, added portfolio block, started clients block

--- js/slider.js
+++ js/slider.js
@@ -2,3 +2,12 @@
  * Created by Anton on 07.09.2015.
  */
 
+(function($){
+    $(document).ready(function(){
+        $('.bxslider').bxSlider({
+            mode: 'fade',
+            captions: true,
+            pager: false
+        });
+    });
+})(jQuery);
